ad734ed1881fc691d5a2e67d4b3fdd4be0a54b58
Quang <qth9368@rit.edu>
2026-03-27 17:58:52 -0400

    sprint 2 demo vid

--- obj/Debug/net9.0/SecureMessenger.AssemblyInfo.cs
+++ obj/Debug/net9.0/SecureMessenger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecureMessenger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba1b3e138799bbcf495a1cccc63909010adbcba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e6bafbafcc773983d3906d898ec8aa306baab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecureMessenger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecureMessenger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
